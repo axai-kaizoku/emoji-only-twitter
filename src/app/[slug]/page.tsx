e95c4cb89376ab87f871b0248b402cc1edc3dbf0
axai-kaizoku <02b3akshay@gmail.com>
2024-07-06 19:05:52 +0530

    profile page�

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { api } from "@/trpc/server";
+import Image from "next/image";
 
 export default async function ProfilePage({
   params,
@@ -11,12 +12,25 @@ export default async function ProfilePage({
     username: userName,
   });
 
-  console.log(data);
-
   return (
-    <div>
-      {/* {params.slug} */}
-      {data?.username}
-    </div>
+    <>
+      <div className="relative h-44 bg-slate-600">
+        <Image
+          src={data?.profilePicture ?? ""}
+          alt={`${data?.username}'s profile picture`}
+          width={128}
+          height={128}
+          className="absolute bottom-0 left-0 -mb-16 ml-4 rounded-full border-4 border-black bg-black dark:border-black"
+        />
+      </div>
+      <div className="h-16"></div>
+      <div className="flex flex-col gap-0.5 p-4">
+        <span className="text-xl font-bold">{data?.name}</span>
+        <span className="text-sm font-extralight text-slate-400">
+          @{data?.username}
+        </span>
+      </div>
+      <div className="w-full border-b border-slate-400" />
+    </>
   );
 }
